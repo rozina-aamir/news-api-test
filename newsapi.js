afcc8dfc52dad39ce954c1335ddd85e722c2eac1
rozina-aamir <rozina.aamir@outlook.com>
2021-04-02 12:21:35 +0100

    Update newsapi.js

--- newsapi.js
+++ newsapi.js
@@ -1,5 +1,4 @@
 var url = 'https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
           'from=2021-04-02&' +
           'sortBy=popularity&' +
           'apiKey=3ac945f900ed4b989311cdf600dc57e8';
